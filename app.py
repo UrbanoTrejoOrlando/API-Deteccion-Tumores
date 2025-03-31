bdb05e01278ba95a5f1130e5cea84dca8e5acae9
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-03-31 09:48:18 -0600

    Update app.py
    Agregando puerto

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def upload_file():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto de Render o 5000 por defecto
+    app.run(host="0.0.0.0", port=port, debug=True)
